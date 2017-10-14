3da511255f3b1bf178065b286a0f3dc1176bb8ee
Wladimir J. van der Laan <laanwj@gmail.com>
2017-10-14 08:38:19 +0000

    Add gem type masks to drm_setup.h
    Fix compile with stock libdrm.
    
    Signed-off-by: Wladimir J. van der Laan <laanwj@gmail.com>

--- src/drm_setup.h
+++ src/drm_setup.h
@@ -19,6 +19,25 @@ struct drm_test_info
     struct etna_cmd_stream *stream;
 };
 
+/** These are not defined by the base drm interface,
+ * but useful for tracking.
+ */
+#ifndef DRM_ETNA_GEM_TYPE_MASK
+#define DRM_ETNA_GEM_TYPE_GEN        0x00000000 /* General */
+#define DRM_ETNA_GEM_TYPE_IDX        0x00000001 /* Index buffer */
+#define DRM_ETNA_GEM_TYPE_VTX        0x00000002 /* Vertex buffer */
+#define DRM_ETNA_GEM_TYPE_TEX        0x00000003 /* Texture */
+#define DRM_ETNA_GEM_TYPE_RT         0x00000004 /* Color render target */
+#define DRM_ETNA_GEM_TYPE_ZS         0x00000005 /* Depth stencil target */
+#define DRM_ETNA_GEM_TYPE_HZ         0x00000006 /* Hierarchical depth render target */
+#define DRM_ETNA_GEM_TYPE_BMP        0x00000007 /* Bitmap */
+#define DRM_ETNA_GEM_TYPE_TS         0x00000008 /* Tile status cache */
+#define DRM_ETNA_GEM_TYPE_TXD        0x00000009 /* Texture descriptor */
+#define DRM_ETNA_GEM_TYPE_IC         0x0000000A /* Instruction cache (Shader code) */
+#define DRM_ETNA_GEM_TYPE_CMD        0x0000000B /* Command buffer */
+#define DRM_ETNA_GEM_TYPE_MASK       0x0000000F
+#endif
+
 extern struct drm_test_info *drm_test_setup(int argc, char **argv);
 
 extern void drm_test_teardown(struct drm_test_info *info);
